e11cc1686725b213c95bc2dd2a6dd0c7f0af1bad
shivam sharma <shivam.upmanyu12@gmail.com>
2021-06-12 11:58:56 +0530

    ini-work

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,17 +1,24 @@
-import { NgModule } from '@angular/core';
+import { NgModule,LOCALE_ID } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { CreatelistComponent } from './createlist/createlist.component';
 import { ShowlistComponent } from './showlist/showlist.component';
+import { BrowserModule } from '@angular/platform-browser';
+
 
 
 @NgModule({
   declarations: [CreatelistComponent, ShowlistComponent],
   imports: [
     CommonModule,
-    AdminRoutingModule
+    AdminRoutingModule,
+    BrowserModule
   ],
+  providers: [ 
+    { provide: LOCALE_ID, useValue: 'hi' } 
+  ],
+
   exports:[
      CreatelistComponent,
      ShowlistComponent
--- src/app/admin/showlist/showlist.component.ts
+++ src/app/admin/showlist/showlist.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { registerLocaleData } from '@angular/common';
+import localeHi from '@angular/common/locales/hi';
+registerLocaleData(localeHi);
 @Component({
   selector: 'app-showlist',
   templateUrl: './showlist.component.html',
@@ -7,6 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShowlistComponent implements OnInit {
 
+
+  currentDate: number = Date.now();
+  name="Shivam Sharma";
+ address="23 pansariyan bohran gali";
+ date = new Date();
+ today:any;
   constructor() { }
 
   ngOnInit() {
